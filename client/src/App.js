10156594d1b1e934ca2e2274a392089d32eed494
sygonsky00 <sygonsky@naver.com>
2021-01-24 23:46:06 +0900

    Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -48,9 +48,22 @@ const styles = theme => ({
 
 class App extends Component{
 
-  state = {
-    customers:"",
-    completed: 0
+  constructor(props){
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
   }
 
   componentDidMount(){
@@ -100,7 +113,7 @@ class App extends Component{
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd/>
+        <CustomerAdd stateRefresh={this.stateRefresh}/>
       </div>
     );
   }
